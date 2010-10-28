8025bd124f4fa33995770a3694d445b224a19fa4
wally <wallyjoe8413@gmail.com>
2010-10-28 03:21:28 -0400

    first scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryPdf::Application.config.secret_token = 'a8d46b8151baeb24c738f4d393e67b0d257c007de860ef9fba952f2d469988c3dce36ffbaff929a0a4589ed139368501ea4da19bc20e7d845e281618937ff42e'
